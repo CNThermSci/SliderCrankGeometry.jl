bd1db93f27d0b45aa6512a61a20413a15934797d
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-06-21 18:46:22 -0300

    UPD the src/simple-crank-rod.jl

--- src/simple-crank-rod.jl
+++ src/simple-crank-rod.jl
@@ -1,39 +1,46 @@
-struct SimpleCrankRod{T <: Base.IEEEFloat}
-    R::T    # crank radius, m
-    L::T    # rod length, m
-    D::T    # piston diameter, m
-    V::T    # minimum volume, m³
-    function SimpleCrankRod(r::T, l::T, d::T, v::T) where {T <: Base.IEEEFloat}
-        @assert(r > zero(T), "Error: R <= 0")
+struct SimpleCrankRod{𝕋 <: Base.IEEEFloat}
+    R::𝕋    # crank radius, m
+    L::𝕋    # rod length, m
+    D::𝕋    # piston diameter, m
+    V::𝕋    # minimum volume, m³
+    function SimpleCrankRod(r::𝕋, l::𝕋, d::𝕋, v::𝕋) where {𝕋 <: Base.IEEEFloat}
+        @assert(r > zero(𝕋), "Error: R <= 0")
         @assert(l > r, "Error: L <= R")
-        @assert(d > zero(T), "Error: D <= 0")
-        @assert(v > zero(T), "Error: V <= 0")
-        return new{T}(r, l, d, v)
+        @assert(d > zero(𝕋), "Error: D <= 0")
+        @assert(v > zero(𝕋), "Error: V <= 0")
+        return new{𝕋}(r, l, d, v)
     end
 end
 
 # External constructors
-function SimpleCrankRod(
-        r::Base.IEEEFloat,
-        l::Base.IEEEFloat,
-        d::Base.IEEEFloat,
-        v::Base.IEEEFloat,
-    )
-    return SimpleCrankRod(promote(r, l, d, v)...)
+function SimpleCrankRod(r::Real, l::Real, d::Real, v::Real)
+    𝕋 = promote_type(typeof.((r, l, d, v))...)
+    𝕋 = 𝕋 <: Base.IEEEFloat ? 𝕋 : Float64
+    return SimpleCrankRod(𝕋.((r, l, d, v))...)
 end
+
 
 # Conversions
 import Base: convert
 
 function convert(
-        ::Type{SimpleCrankRod{T}},
+        ::𝕋ype{SimpleCrankRod{𝕋}},
         x::SimpleCrankRod{S}
-    ) where {T <: Base.IEEEFloat, S <: Base.IEEEFloat}
+    ) where {𝕋 <: Base.IEEEFloat, S <: Base.IEEEFloat}
     return SimpleCrankRod(
-        T(x.R), T(x.L), T(x.D), T(x.V)
+        𝕋(x.R), 𝕋(x.L), 𝕋(x.D), 𝕋(x.V)
     )
 end
 
 # Promotions
+import Base: promote_rule
 
+function promote_rule(
+        ::𝕋ype{SimpleCrankRod{𝕋}},
+        ::𝕋ype{SimpleCrankRod{𝕊}}
+    ) where {𝕋 <: Base.IEEEFloat, 𝕊 <: Base.IEEEFloat}
+    return SimpleCrankRod{promote_type(𝕋, 𝕊)}
+end
+
+# Export
 export SimpleCrankRod

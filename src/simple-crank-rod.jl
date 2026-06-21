d9df426e11ad5990cb0a0d9a5445e9cbe4fca4e7
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-06-21 13:09:48 -0300

    UPD the src/simple-crank-rod.jl

--- src/simple-crank-rod.jl
+++ src/simple-crank-rod.jl
@@ -1,8 +1,15 @@
 struct SimpleCrankRod{T<:Base.IEEEFloat}
-    R::T
-    L::T
-    D::T
-    V::T
+    R::T    # crank radius, m
+    L::T    # rod length, m
+    D::T    # piston diameter, m
+    V::T    # minimum volume, m³
+    function SimpleCrankRod(r::T, l::T, d::T, v::T) where {T<:Base.IEEEFloat}
+        @assert(r > zero(T), "Error: R <= 0")
+        @assert(l > r,       "Error: L <= R")
+        @assert(d > zero(T), "Error: D <= 0")
+        @assert(v > zero(T), "Error: V <= 0")
+        new(r, l, d, v)
+    end
 end
 
 export SimpleCrankRod

a25eaa9359165a97c8f30923a2820e41eb061bca
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-06-21 19:11:25 -0300

    UPD the src/simple-crank-rod.jl

--- src/simple-crank-rod.jl
+++ src/simple-crank-rod.jl
@@ -1,9 +1,9 @@
-struct SimpleCrankRod{ℙ <: Base.IEEEFloat}
+struct SimpleCR{ℙ <: Base.IEEEFloat}
     R::ℙ    # crank radius, m
     L::ℙ    # rod length, m
     D::ℙ    # piston diameter, m
     V::ℙ    # minimum volume, m³
-    function SimpleCrankRod(r::ℙ, l::ℙ, d::ℙ, v::ℙ) where {ℙ <: Base.IEEEFloat}
+    function SimpleCR(r::ℙ, l::ℙ, d::ℙ, v::ℙ) where {ℙ <: Base.IEEEFloat}
         @assert(r > zero(ℙ), "Error: R <= 0")
         @assert(l > r, "Error: L <= R")
         @assert(d > zero(ℙ), "Error: D <= 0")
@@ -13,23 +13,23 @@ struct SimpleCrankRod{ℙ <: Base.IEEEFloat}
 end
 
 # External constructors
-function SimpleCrankRod{ℙ}(r::Real, l::Real, d::Real, v::Real) where {ℙ <: Base.IEEEFloat}
-    return SimpleCrankRod(ℙ.((r, l, d, v))...)
+function SimpleCR{ℙ}(r::Real, l::Real, d::Real, v::Real) where {ℙ <: Base.IEEEFloat}
+    return SimpleCR(ℙ.((r, l, d, v))...)
 end
 
-function SimpleCrankRod(r::Real, l::Real, d::Real, v::Real)
+function SimpleCR(r::Real, l::Real, d::Real, v::Real)
     ℙ = promote_type(typeof.((r, l, d, v))...)
     ℙ = ℙ <: Base.IEEEFloat ? ℙ : Float64
-    return SimpleCrankRod(ℙ.((r, l, d, v))...)
+    return SimpleCR(ℙ.((r, l, d, v))...)
 end
 
-function SimpleCrankRod(
+function SimpleCR(
         r::Quantity{T, Unitful.𝐋} where {T <: Real},
         l::Quantity{T, Unitful.𝐋} where {T <: Real},
         d::Quantity{T, Unitful.𝐋} where {T <: Real},
         v::Quantity{T, Unitful.𝐋^3} where {T <: Real},
     )
-    return SimpleCrankRod(
+    return SimpleCR(
         uconvert(u"m", r).val,
         uconvert(u"m", l).val,
         uconvert(u"m", d).val,
@@ -41,10 +41,10 @@ end
 import Base: convert
 
 function convert(
-        ::Type{SimpleCrankRod{ℙ}},
-        x::SimpleCrankRod{ℚ}
+        ::Type{SimpleCR{ℙ}},
+        x::SimpleCR{ℚ}
     ) where {ℙ <: Base.IEEEFloat, ℚ <: Base.IEEEFloat}
-    return SimpleCrankRod(
+    return SimpleCR(
         ℙ(x.R), ℙ(x.L), ℙ(x.D), ℙ(x.V)
     )
 end
@@ -53,11 +53,11 @@ end
 import Base: promote_rule
 
 function promote_rule(
-        ::Type{SimpleCrankRod{ℙ}},
-        ::Type{SimpleCrankRod{ℚ}}
+        ::Type{SimpleCR{ℙ}},
+        ::Type{SimpleCR{ℚ}}
     ) where {ℙ <: Base.IEEEFloat, ℚ <: Base.IEEEFloat}
-    return SimpleCrankRod{promote_type(ℙ, ℚ)}
+    return SimpleCR{promote_type(ℙ, ℚ)}
 end
 
 # Export
-export SimpleCrankRod
+export SimpleCR

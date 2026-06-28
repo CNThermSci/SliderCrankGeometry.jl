1db163822f5dc2ecd8a6489d84e54ce17c475b07
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-06-28 14:12:57 -0300

    UPD the test/MonoCylinder.test.jl

--- test/MonoCylinder.test.jl
+++ test/MonoCylinder.test.jl
@@ -14,20 +14,20 @@ end
 @testset "simple-crank-rod.test.jl: inner constructor return types                " begin
     for ℙ in union2vec(Base.IEEEFloat)
         RLDr = ℙ.((1, 2, 1, 2))
-        @test SimpleCR(RLDr...) isa SimpleCR{ℙ}
+        @test MonoCylinder(RLDr...) isa MonoCylinder{ℙ}
     end
 end
 
 @testset "simple-crank-rod.test.jl: inner constructor validations                 " begin
     for ℙ in union2vec(Base.IEEEFloat)
         RLDr = ℙ.((0, 2, 1, 2))
-        @test_throws "Error: R <= 0" SimpleCR(RLDr...)
+        @test_throws "Error: R <= 0" MonoCylinder(RLDr...)
         RLDr = ℙ.((1, 1, 1, 2))
-        @test_throws "Error: L <= R" SimpleCR(RLDr...)
+        @test_throws "Error: L <= R" MonoCylinder(RLDr...)
         RLDr = ℙ.((1, 2, 0, 2))
-        @test_throws "Error: D <= 0" SimpleCR(RLDr...)
+        @test_throws "Error: D <= 0" MonoCylinder(RLDr...)
         RLDr = ℙ.((1, 2, 1, 1))
-        @test_throws "Error: r <= 1" SimpleCR(RLDr...)
+        @test_throws "Error: r <= 1" MonoCylinder(RLDr...)
     end
 end
 
@@ -35,30 +35,30 @@ end
     # Set type conversion outer constructor
     for ℙ in union2vec(Base.IEEEFloat)
         RLDr = (1, 2//1, BigFloat("1"), ℯ)
-        @test SimpleCR{ℙ}(RLDr...) isa SimpleCR{ℙ}
+        @test MonoCylinder{ℙ}(RLDr...) isa MonoCylinder{ℙ}
         RLDr = (1u"cm", 2//1 * u"cm", BigFloat("1")u"cm", 200 * ℯ * u"percent")
-        @test SimpleCR{ℙ}(RLDr...) isa SimpleCR{ℙ}
+        @test MonoCylinder{ℙ}(RLDr...) isa MonoCylinder{ℙ}
     end
     # Promotion type conversion outer constructor
     RLDr = (1, 2//1, BigFloat("1"), 2//1)
-    @test SimpleCR(RLDr...) isa SimpleCR{Float64}
+    @test MonoCylinder(RLDr...) isa MonoCylinder{Float64}
     RLDr = (1u"cm", 2//1 * u"cm", BigFloat("1")u"cm", 200//1 * u"percent")
-    @test SimpleCR(RLDr...) isa SimpleCR{Float64}
+    @test MonoCylinder(RLDr...) isa MonoCylinder{Float64}
     for ℙ in union2vec(Base.IEEEFloat)
         RLDr = (1, 2//1, 0x01, ℙ(ℯ))
-        @test SimpleCR(RLDr...) isa SimpleCR{ℙ}
+        @test MonoCylinder(RLDr...) isa MonoCylinder{ℙ}
         RLDr = (1u"cm", 2//1 * u"cm", 0x01 * u"cm", ℙ(200 * ℯ * u"percent"))
-        @test SimpleCR(RLDr...) isa SimpleCR{ℙ}
+        @test MonoCylinder(RLDr...) isa MonoCylinder{ℙ}
     end
 end
 
 function suffKwargsTest(; kwargs...)
     KW = (; kwargs...) # Builds back NamedTuple from Base.Pairs
     # Sufficient kwargs must pass
-    @test SimpleCR(; KW...) isa SimpleCR
+    @test MonoCylinder(; KW...) isa MonoCylinder
     for kw in [Base.structdiff(KW, NamedTuple{(k,)}) for k in keys(KW)]
         # Incomplete kwargs must fail
-        @test_throws AssertionError SimpleCR(; kw...)
+        @test_throws AssertionError MonoCylinder(; kw...)
     end
 end
 
